80227c164d55b8fe87a11c7ccb2b37a024b91bf8
sginko <s.ginkota@gmail.com>
2024-03-14 14:42:09 +0100

    task 3

--- src/test/java/pl/akademiaspecjalistowit/streamapiinpractice/zadanie3/Zadanie3.java
+++ src/test/java/pl/akademiaspecjalistowit/streamapiinpractice/zadanie3/Zadanie3.java
@@ -3,29 +3,42 @@ package pl.akademiaspecjalistowit.streamapiinpractice.zadanie3;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Zadanie3 {
 
     /**
      * ZACZNIJ OD NAPISANIA ASERCJI!
-     *
+     * <p>
      * Mając listę pracowników (obiekty z polami name i salary), znajdź pierwszych
      * 5 pracowników z najwyższą pensją i zwróć ich imiona w posortowanej liście alfabetycznie.
      */
     @Test
     void zadanie3() {
-
+        //given
         List<Employee> employees = List.of(
-            new Employee("John Doe", 70000),
-            new Employee("Jane Smith", 80000),
-            new Employee("Ethan Black", 90000),
-            new Employee("Emma White", 85000),
-            new Employee("Michael Brown", 50000)
+                new Employee("John Doe", 70000),
+                new Employee("Jane Smith", 80000),
+                new Employee("Ethan Black", 90000),
+                new Employee("Emma White", 85000),
+                new Employee("Michael Brown", 50000)
         );
 
-        List<String> topEarners = null;
-        System.out.println("Top zarabiający pracownicy: " + topEarners);
+        //when
+        List<String> topEarners = groupEmployee(employees);
 
+        //then
+        assertThat(topEarners).containsExactly("Ethan Black", "Emma White", "Jane Smith", "John Doe", "Michael Brown");
+    }
+
+    private List<String> groupEmployee(List<Employee> employees) {
+        return employees.stream()
+                .sorted(Comparator.comparing((Employee e) -> e.getSalary()).reversed().thenComparing(e -> e.getName()))
+                .limit(5)
+                .map(e -> e.getName())
+                .collect(Collectors.toList());
     }
 }
